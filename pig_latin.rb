e897a5a5b3b9cd0a761668e7dd0684cf678f7f7d
Richard Vogt <richvogtjr@gmail.com>
2013-03-28 15:40:41 -0400

    pig latin translates two words

--- pig_latin.rb
+++ pig_latin.rb
@@ -11,30 +11,45 @@
 
 def translate(str)
 
-  if str =~ /\b\s\b/
-    a = str.split(/\s/)
-    a.each do |string|
-      if string =~ /\A[b-df-hj-np-tv-z]{2}/
-        string.concat(str[0] + str[1] + "ay")
-        trans_str = string.reverse.byteslice(0..-3)
-        trans_str.reverse 
-      elsif str =~ /\A[b-df-hj-np-tv-z]{1}/
-        str.concat(str[0] + "ay")
-        trans_str = string.reverse.chop
-        trans_str.reverse     
-      end #end if
-    end #end do
-    a.join(' ')
 
-  elsif str =~ /\A[aeiou]/
+  if str =~ /\b\s\b/ # string has at least two words separated by a whitespace
+    a = str.split(/\s/)
+    temp = []
+
+    a.each do |string|
+
+      if string =~ /\A[b-df-hj-np-tv-z]{2}/ # check for two leading consonants
+        string.concat(string[0] + string[1] + "ay")
+        translated_str = string.reverse.byteslice(0..-3)
+        temp << translated_str.reverse
+
+      elsif string =~ /\A[b-df-hj-np-tv-z]{1}/ #check for one leading consonant
+        string.concat(string[0] + "ay")
+        translated_str = string.reverse.chop
+        translated_str.reverse!     
+        temp << translated_str
+
+      else # string has leading vowel
+        temp << string.concat("ay")
+
+      end #end if for inner match
+
+    end #end do
+
+    temp.join(' ')
+
+
+
+
+  elsif str =~ /\A[aeiou]/ # word leads with a vowel
     str.concat("ay")
 
-  elsif str =~ /\A[b-df-hj-np-tv-z]{2}/
+  elsif str =~ /\A[b-df-hj-np-tv-z]{2}/ # word leads with two consonants
     str.concat(str[0] + str[1] + "ay")
     trans_str = str.reverse.byteslice(0..-3)
     trans_str.reverse 
 
-  elsif str =~ /\A[b-df-hj-np-tv-z]{1}/
+  elsif str =~ /\A[b-df-hj-np-tv-z]{1}/ # words leads with a consonant
     str.concat(str[0] + "ay")
     trans_str = str.reverse.chop
     trans_str.reverse
