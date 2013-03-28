3e3a763f54af2eb9fd1bb0f319f88339b0d947e7
Richard Vogt <richvogtjr@gmail.com>
2013-03-28 12:31:32 -0400

    add multiple words to Pig Latin

--- pig_latin.rb
+++ pig_latin.rb
@@ -10,16 +10,34 @@
 # append the leading consonants if any + ay 
 
 def translate(str)
-  if str =~ /\A[aeiou]/
+
+  if str =~ /\b\s\b/
+    a = str.split(/\s/)
+    a.each do |string|
+      if string =~ /\A[b-df-hj-np-tv-z]{2}/
+        string.concat(str[0] + str[1] + "ay")
+        trans_str = string.reverse.byteslice(0..-3)
+        trans_str.reverse 
+      elsif str =~ /\A[b-df-hj-np-tv-z]{1}/
+        str.concat(str[0] + "ay")
+        trans_str = string.reverse.chop
+        trans_str.reverse     
+      end #end if
+    end #end do
+    a.join(' ')
+
+  elsif str =~ /\A[aeiou]/
     str.concat("ay")
-   elsif str =~ /\A[b-df-hj-np-tv-z]{2}/
+
+  elsif str =~ /\A[b-df-hj-np-tv-z]{2}/
     str.concat(str[0] + str[1] + "ay")
     trans_str = str.reverse.byteslice(0..-3)
     trans_str.reverse 
-   elsif str =~ /\A[b-df-hj-np-tv-z]{1}/
+
+  elsif str =~ /\A[b-df-hj-np-tv-z]{1}/
     str.concat(str[0] + "ay")
     trans_str = str.reverse.chop
     trans_str.reverse
 
-  end
+  end #end if
 end
