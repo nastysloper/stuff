6f763ddd617b2a427d1e9bd5a66c71b500d8be1d
Richard Vogt <richvogtjr@gmail.com>
2013-03-28 12:08:55 -0400

    cool regx program added

---
+++ pig_latin.rb
@@ -0,0 +1,25 @@
+#pig latin program
+#
+# concat()
+# here's the algorithm:
+# match the string with a regx
+# assign a variable that corresponds with the number of leading consonants
+# reverse the string
+# chop it
+# reverse it again to put it back normally
+# append the leading consonants if any + ay 
+
+def translate(str)
+  if str =~ /\A[aeiou]/
+    str.concat("ay")
+   elsif str =~ /\A[b-df-hj-np-tv-z]{2}/
+    str.concat(str[0] + str[1] + "ay")
+    trans_str = str.reverse.byteslice(0..-3)
+    trans_str.reverse 
+   elsif str =~ /\A[b-df-hj-np-tv-z]{1}/
+    str.concat(str[0] + "ay")
+    trans_str = str.reverse.chop
+    trans_str.reverse
+
+  end
+end
