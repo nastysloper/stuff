eeceb42bf6238902078a393bfe4522c4f184f66b
Richard Vogt <richvogtjr@gmail.com>
2013-04-01 18:07:54 -0400

    pad_array complete

--- pad_array.rb
+++ pad_array.rb
@@ -1,16 +1,20 @@
 class Array
   def pad!(min_size, value = nil)
-    i = min_size - self.size
-    0.upto(i) do
-      self.push("cat")
+    (min_size - self.size).times do
+      self << value
     end
-  end
-  
-  #def pad(min_size, value = nil)
-
-  #end
-end
+    self
+  end # end pad! def
+    
+  def pad(min_size, value = nil)
+    return_array = self
+    (min_size - self.size).times do
+      return_array << value
+    end
+    return_array
+  end # end pad def
+end # end class
 
 arr1 = [1, 2, 3]
 p arr1
-p arr1.pad!(5)
+puts arr1.pad(8, 'apple').inspect
