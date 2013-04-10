12d75013c20bdcc72610346cf33086eceab636f4
Richard Vogt <richvogtjr@gmail.com>
2013-04-10 16:10:41 -0400

    Reverse Polish that allows for negative numbers

--- rpn1.rb
+++ rpn1.rb
@@ -1,53 +1,38 @@
 class RPNCalculator
-
-  def evaluate(arg)
-    arr = arg.split
-    puts "This is arr: #{arr}"
-    length = arr.size
-
-    while length >= 3
-      op = arr.index {|x| x =~ /[+-]/}
-      operator = op
-      puts "This is the operator: #{operator}"
-      num1 = op - 1
-      num2 = op - 20
-      puts "This is num1: #{num1}"
-      puts "This is num2: #{num2}"
-      if arr[op] =~ /[+]/
-      #  puts "#{arr} #{num2} #{num2} end of this"
-        arr[0] << addition(arr, num1, num2)
-      elsif arr[op] =~ /[-]/
-        arr[0] << subtract(arr, num1, num2)
-      elsif arr[op] =~ /[*]/
-        arr[0] << multiply(arr, num1, num2)
-      end # end if
-      length -= 3
-    end # end while
-
-    if(arr.size == 1)
-      return arg.to_i
+  
+  def evaluate arg
+    array = arg.split
+    i = 0
+    result = 0
+    until array[i] =~ /[+*-]/ or array.size < 3 and array[i] !~ /-\d/
+      i += 1
+      puts "Here => #{array[i]}"
+      if array[i] =~ /[+*-]/ and array[i] !~ /-\d/
+        operator = array[i]
+        num2 = array[i-1].to_i
+        num1 = array[i-2].to_i
+        if operator =~ /[+]/
+          result = num1 + num2
+        end
+        if operator =~ /[*]/
+          result = num1 * num2
+        end
+        if operator =~ /[-]/
+          result = num1 - num2
+        end
+        # now reset the array
+        array[i-2] = result.to_s
+        array.delete_at(i)
+        array.delete_at(i-1)
+        i = 0
+        if array.size >= 3
+          evaluate(array.join(" "))
+        end
+      end
     end
-
-  end # end evaluate
-
-
-  def multiply(arr, num1, num2)
-    arr[num1].to_i * arr[num2].to_i
+    array[0].to_i
   end
+end
 
-
-  def addition(arr, num1, num2)
-    arr[num1].to_i + arr[num2].to_i
-  end
-
-  def subtract(arr, num1, num2)
-    arr[num1].to_i - arr[num2].to_i
-  end
-
-end # end class RPNcalculator
-
-
-calc = RPNCalculator.new
-p calc.evaluate("7 2 +")
-#p calc.evaluate("5 1 -")
-#p calc.evaluate("1 2 + 3 4 +")
+molg = RPNCalculator.new
+p molg.evaluate("1 -6 5 + +")
