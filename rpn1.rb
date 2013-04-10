2672a4bddb5c2c63e764238be12c0a3321c30d72
Richard Vogt <richvogtjr@gmail.com>
2013-04-09 22:04:16 -0400

    add 1st draft of Reverse Polish Calculator

--- rpn1.rb
+++ rpn1.rb
@@ -2,14 +2,19 @@ class RPNCalculator
 
   def evaluate(arg)
     arr = arg.split
+    puts "This is arr: #{arr}"
     length = arr.size
 
     while length >= 3
       op = arr.index {|x| x =~ /[+-]/}
       operator = op
+      puts "This is the operator: #{operator}"
       num1 = op - 1
-      num2 = op - 2
+      num2 = op - 20
+      puts "This is num1: #{num1}"
+      puts "This is num2: #{num2}"
       if arr[op] =~ /[+]/
+      #  puts "#{arr} #{num2} #{num2} end of this"
         arr[0] << addition(arr, num1, num2)
       elsif arr[op] =~ /[-]/
         arr[0] << subtract(arr, num1, num2)
@@ -44,5 +49,5 @@ end # end class RPNcalculator
 
 calc = RPNCalculator.new
 p calc.evaluate("7 2 +")
-p calc.evaluate("5 1 -")
-p calc.evaluate("1 2 + 3 4 +")
+#p calc.evaluate("5 1 -")
+#p calc.evaluate("1 2 + 3 4 +")
